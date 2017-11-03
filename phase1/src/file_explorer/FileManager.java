410720eddc5474ec7f1eb62fad014197adc64709
Elliot Ford <elliot.ford@mail.utoronto.ca>
2017-11-03 09:38:04 -0400

    Initial commit of FileManager, created constructor and a private recursive method to fill arraylist of non-directories.

---
+++ phase1/src/file_explorer/FileManager.java
@@ -0,0 +1,25 @@
+package file_explorer;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileManager {
+    List<File> nonDirectoryFileList;
+
+    public FileManager(String root) {
+        nonDirectoryFileList = getAllLeafs(new File(root));
+    }
+
+    private List<File> getAllLeafs(File root) {
+        List<File> ret = new ArrayList<>();
+        if(root.isDirectory() && root.listFiles() != null) {
+            for(File subFile: root.listFiles()) {
+                ret.addAll(getAllLeafs(subFile));
+            }
+        } else {
+            ret.add(root);
+        }
+        return ret;
+    }
+}
