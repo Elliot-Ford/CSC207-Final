0ee21f074ac4bb4643443427b0a3666fc31dc797
Elliot Ford <elliot.ford@mail.utoronto.ca>
2017-11-09 21:24:18 -0500

    fixed error in Image.addTag and a boolean in Image and Tag. Reformated Image and Tag

--- phase1/src/image_tag_explorer/Image.java
+++ phase1/src/image_tag_explorer/Image.java
@@ -8,93 +8,90 @@ import java.util.Stack;
 
 public class Image {
 
-    private File imageFile;
-    private List<Tag> tags;
-    private Stack<String> previousNames;
+  private File imageFile;
+  private List<Tag> tags;
+  private Stack<String> previousNames;
 
-    /**
-     * Constructs an abstract image with a referring file with no tags.
-     * @param imageFile the abstract file representation of the image file
-     */
-    public Image(File imageFile) {
-        this(imageFile, new Tag[0]);
+  /**
+   * Constructs an abstract image with a referring file with no tags.
+   *
+   * @param imageFile the abstract file representation of the image file
+   */
+  public Image(File imageFile) {
+    this(imageFile, new Tag[0]);
+  }
+
+  public Image(File imageFile, Tag[] tags) {
+    this.imageFile = imageFile;
+    this.tags = new ArrayList<>(Arrays.asList(tags));
+    previousNames = new Stack<>();
+
+    // Adding Tags into the Image name.
+    for (Tag tag : tags) {
+      previousNames.add("@" + tag.getName());
+    }
+    // TODO: if there are "@" tags on the image name that aren't in the given Tag[] array throw an
+    // custom error.
+    // why do we need to check this if we are constructing a new Image object?
+  }
+
+  /**
+   * adds a given tag to the Image, modifies the image filename to match the new change
+   *
+   * @param newTag the tag to add to the image
+   */
+  public void addTag(Tag newTag) {
+    boolean here = false;
+    // Checking if this Tag is new to this Image
+    for (Tag tag : tags) {
+      if (tag.getName().equals(newTag.getName())) {
+        here = true;
+      }
     }
 
-    public Image(File imageFile, Tag[] tags) {
-        this.imageFile = imageFile;
-        this.tags = new ArrayList<>(Arrays.asList(tags));
-        previousNames = new Stack<>();
+    if (!here) {
+      rename(String.format("%s @ %s", imageFile.getName(), newTag.getName()));
+      tags.add(newTag);
+      newTag.addImage(this);
+    }
+  }
 
-        // Adding Tags into the Image name.
-        for (Tag tag : tags) {
-            previousNames.add("@" + tag.getName());
-        }
-        //TODO: if there are "@" tags on the image name that aren't in the given Tag[] array throw an custom error.
-        // why do we need to check this if we are constructing a new Image object?
+  /**
+   * tries to remove a given tag from the Image, modifies the image filename to match the new change
+   *
+   * @param tag the tag to try and remove from the image
+   * @return true if tag removal was successful, false if not.
+   */
+  public void removeTag(Tag tag) {
+    boolean here = false;
+    // Checking if this Tag is in this Image
+    for (String tagName : previousNames) {
+      if (tagName.equals(tag.getName())) {
+        here = true;
+      }
     }
 
-    /**
-     * adds a given tag to the Image, modifies the image filename
-     * to match the new change
-     *
-     * @param newTag the tag to add to the image
-     */
-    public void addTag(Tag newTag) {
-        boolean notHere = true;
-        // Checking if this Tag is new to this Image
-        for (String tagName : previousNames) {
-            if (tagName.equals(newTag.getName())) {
-                notHere = false;
-            }
-        }
-
-        if (notHere) {
-            previousNames.add("@" + newTag.getName());
-            tags.add(newTag);
-            newTag.addImage(this);
-        }
+    // If this Tag is in this Image, then remove it.
+    if (here) {
+      tags.remove(tag);
+      previousNames.remove("@" + tag.getName());
+      tag.removeImage(this);
+      // TODO: this return value of tag.removeImage(this) is not used in this call
     }
+  }
 
-    /**
-     * tries to remove a given tag from the Image, modifies the image filename
-     * to match the new change
-     *
-     * @param tag the tag to try and remove from the image
-     * @return true if tag removal was successful, false if not.
-     */
-    public boolean removeTag(Tag tag) {
-        boolean notHere = true;
-        // Checking if this Tag is in this Image
-        for (String tagName : previousNames) {
-            if (tagName.equals(tag.getName())) {
-                notHere = false;
-            }
-        }
+  public void rename(String newName) {
+    previousNames.push(imageFile.getName());
+    imageFile.renameTo(new File(imageFile.getParentFile(), newName));
+    // TODO: write a tester that makes sure this method does what's expected.
+  }
 
-        // If this Tag is in this Image, then remove it.
-        if (!notHere) {
-            tags.remove(tag);
-            previousNames.remove("@" + tag.getName());
-            tag.removeImage(this);
-            //TODO: this return value of tag.removeImage(this) is not used in this call
-        }
-
-        // If !notHere is true, then the removal was successful.
-        return !notHere;
-    }
-
-    public void rename(String newName) {
-        previousNames.push(imageFile.getName());
-        imageFile.renameTo(new File(imageFile.getParentFile(), newName));
-        //TODO: write a tester that makes sure this method does what's expected.
-    }
-
-    /**
-     * returns the name of an Image.
-     *
-     * @return the name of the Image.
-     */
-    public String getName() {
-        return imageFile.getName();
-    }
+  /**
+   * returns the name of an Image.
+   *
+   * @return the name of the Image.
+   */
+  public String getName() {
+    return imageFile.getName();
+  }
 }
--- phase1/src/image_tag_explorer/Tag.java
+++ phase1/src/image_tag_explorer/Tag.java
@@ -6,54 +6,51 @@ import java.util.List;
 
 public class Tag {
 
-    private List<Image> images;
-    private String name;
+  private List<Image> images;
+  private String name;
 
-    public Tag(String name) {
-        this(name, new Image[0]);
+  public Tag(String name) {
+    this(name, new Image[0]);
+  }
+
+  public Tag(String name, Image[] images) {
+    this.name = name;
+    this.images = new ArrayList<>(Arrays.asList(images));
+  }
+
+  public void addImage(Image newImage) {
+    boolean here = false;
+    // Checking if this Image is new to this Tag
+    for (Image image : images) {
+      if (image.getName().equals(newImage.getName())) {
+        here = true;
+      }
     }
 
-    public Tag(String name, Image[] images) {
-        this.name = name;
-        this.images = new ArrayList<>(Arrays.asList(images));
+    if (!here) {
+      images.add(newImage);
+      newImage.addTag(this);
+    }
+  }
+
+  public void removeImage(Image image) {
+    boolean here = false;
+    // Checking if this Image is in this Tag
+    for (Image existImage : images) {
+      if (existImage.getName().equals(image.getName())) {
+        here = true;
+      }
     }
 
-    public void addImage(Image newImage) {
-        boolean notHere = true;
-        // Checking if this Image is new to this Tag
-        for (Image image : images) {
-            if (image.getName().equals(newImage.getName())) {
-                notHere = false;
-            }
-        }
-
-        if (notHere) {
-            images.add(newImage);
-            newImage.addTag(this);
-        }
+    // If this Image is in this Tag, then remove it.
+    if (here) {
+      images.remove(image);
+      image.removeTag(this);
+      // TODO: this return value of image.removeTag(this) is not used in this call
     }
+  }
 
-    public boolean removeImage(Image image) {
-        boolean notHere = true;
-        // Checking if this Image is in this Tag
-        for (Image existImage : images) {
-            if (existImage.getName().equals(image.getName())) {
-                notHere = false;
-            }
-        }
-
-        // If this Image is in this Tag, then remove it.
-        if (!notHere) {
-            images.remove(image);
-            image.removeTag(this);
-            //TODO: this return value of image.removeTag(this) is not used in this call
-        }
-
-        // If !notHere is true, then the removal was successful.
-        return !notHere;
-    }
-
-    public String getName() {
-        return this.name;
-    }
+  public String getName() {
+    return this.name;
+  }
 }
