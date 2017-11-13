8bd40aeb39cd2e21fc4eb483f124a221bb6fec08
Elliot Ford <elliot.ford@mail.utoronto.ca>
2017-11-13 16:01:19 -0500

    Code affected: TagManager
    Issue: TagManager is empty.
    
    Fix: Created some constructors, getters, adders and removers.

--- phase1/src/image_tag_explorer/TagManager.java
+++ phase1/src/image_tag_explorer/TagManager.java
@@ -1,4 +1,31 @@
 package image_tag_explorer;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class TagManager {
+	public List<Tag> tags;
+
+	public TagManager() {
+		this(new Tag[0]);
+	}
+
+	public TagManager(Tag[] tags) {
+		this.tags = new ArrayList<>(Arrays.asList(tags));
+
+	}
+
+	public void addTag(Tag newTag) {
+		tags.add(newTag);
+	}
+
+	public void removeTag(Tag tag) {
+		tags.remove(tag);
+	}
+
+	public Tag[] getTags() {
+		return (Tag[]) tags.toArray();
+	}
 }
