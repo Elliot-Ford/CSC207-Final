ae5d0778439361febf85a30ce153b3a68e26028a
Elliot Ford <elliot.ford@mail.utoronto.ca>
2017-11-13 18:11:47 -0500

    Added more code to ModelManager

--- phase1/src/image_tag_model/ModelManager.java
+++ phase1/src/image_tag_model/ModelManager.java
@@ -1,15 +1,26 @@
 package image_tag_model;
 
 
+import file_explorer.FileManager;
 import image_tag_explorer.ImageManager;
 import image_tag_explorer.TagManager;
+
+import java.io.File;
 
 public class ModelManager {
 	ImageManager imageManager;
 	TagManager tagManager;
+	FileManager fileManager;
 
 	public ModelManager() {
 		imageManager = new ImageManager();
+		tagManager = new TagManager();
+		fileManager = new FileManager();
+	}
+
+	public ModelManager(File root) {
+		this();
+		fileManager.getFiles(root,"*.jpg");
 	}
 
 }
