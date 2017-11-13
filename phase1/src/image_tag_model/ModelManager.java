de5be9b1d1e5f3777eb6dd597552bd7aa981199d
Elliot Ford <elliot.ford@mail.utoronto.ca>
2017-11-13 15:07:34 -0500

    Code affected: ModelManager
    Issue: need a class to manage the model.
    
    Fix: initial commit of ModelManager.

---
+++ phase1/src/image_tag_model/ModelManager.java
@@ -0,0 +1,15 @@
+package image_tag_model;
+
+
+import image_tag_explorer.ImageManager;
+import image_tag_explorer.TagManager;
+
+public class ModelManager {
+	ImageManager imageManager;
+	TagManager tagManager;
+
+	public ModelManager() {
+		imageManager = new ImageManager();
+	}
+
+}
