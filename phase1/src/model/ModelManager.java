7b7b15e99492e4e9899b8b73f9eb45589fe3e35f
Elliot Ford <elliot.ford@mail.utoronto.ca>
2017-11-16 02:40:33 -0500

    Modified ModelManager to match new changes to FileManger

--- phase1/src/model/ModelManager.java
+++ phase1/src/model/ModelManager.java
@@ -6,10 +6,6 @@ import model.image_tag_explorer.ImageManager;
 import model.image_tag_explorer.TagManager;
 
 import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class ModelManager {
   /** String to match all tagable files */
@@ -23,20 +19,13 @@ public class ModelManager {
   public ModelManager() {
     imageManager = new ImageManager();
     tagManager = new TagManager();
-    fileManager = new FileManager();
+    fileManager = new FileManager("");
   }
 
   public ModelManager(String path) {
     this();
     // TODO: add a way to load a serializable file if it exists.
-    fileManager.getFiles(path, "*.jpg");
-
-    for (File file : fileManager.getFiles(path, FILE_MATCH_STRING)) {
-      try {
-        imageManager.addImage(new Image(file));
-      } catch (Exception e) {
-        // TODO: we should catch when Image has tags that are not in tagManager
-      }
+    for (File file : fileManager.getAllFiles(FILE_MATCH_STRING)) {
     }
   }
 }
