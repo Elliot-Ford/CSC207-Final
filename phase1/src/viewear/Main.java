f9a074ceb27f1c92a6d5473966c3a8cef335ba5c
Shlok Somani <shloksomani@ymail.com>
2017-11-16 00:22:39 -0500

    Changed the package name to viewer.

---
+++ phase1/src/viewear/Main.java
@@ -0,0 +1,25 @@
+package viewear;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public Stage window;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
