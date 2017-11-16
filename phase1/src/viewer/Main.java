7c20ed8fa760f6f7e9efe539ffd2c055e18f6723
Shlok Somani <shloksomani@ymail.com>
2017-11-16 01:51:37 -0500

    In viewer created a main class which is extended by Application and implemented start method.

--- phase1/src/viewer/Main.java
+++ phase1/src/viewer/Main.java
@@ -1,12 +1,6 @@
-package viewear;
+package viewer;
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
