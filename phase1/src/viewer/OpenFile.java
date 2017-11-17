e0cc612963a2acf65fba4db277c2d043ca0400e8
Shlok Somani <shloksomani@ymail.com>
2017-11-17 17:55:18 -0500

    In OpenFile.java created the method openFile().

--- phase1/src/viewer/OpenFile.java
+++ phase1/src/viewer/OpenFile.java
@@ -1,19 +1,32 @@
 package viewer;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
-import javafx.stage.Stage;
+import java.awt.Desktop;
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
-public class OpenFile {
+public final class OpenFile{
+    Stage window;
+
+    public void openFile(Stage stage) {
+        window = stage;
+        window.show();
+        GridPane newGrid = new GridPane();
+        Scene scene2 = new Scene(newGrid, 1020, 720);
+        window.setScene(scene2);
+        window.setTitle("Lets do Tagging");
+    }
 }
