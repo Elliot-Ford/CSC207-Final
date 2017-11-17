12e480940f538c25cbfd0cab078ceb099e336523
guycalledjeremy <guycalledjeremy@gmail.com>
2017-11-16 23:40:48 -0500

    package reStructure;
    public class ImageFile {
    }

--- phase1/src/reStructure/ImageFileManager.java
+++ phase1/src/reStructure/ImageFileManager.java
@@ -1,4 +1,26 @@
 package reStructure;
 
+import reStructure.ImageFile;
+import reStructure.TagManager;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class ImageFileManager {
+    //    private static final String SERIALIZED_IMAGES_FILENAME = ".images.ser";
+    /** the name of a serialized file of tags */
+    private static final String SERIALIZED_TAGS_FILENAME = ".tags.ser";
+    /** String to match all tagable files */
+    private static final String FILE_MATCH_STRING = ".*[.](jpg|png|gif|bmp)";
+
+    /** the root of the directory */
+    private File root;
+
+    public ImageFileManager(String path) {
+        
+    }
+
 }
