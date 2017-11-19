cd71ddac47cdce990a162ec5b08c616ce104dff5
Shlok Somani <shloksomani@ymail.com>
2017-11-19 15:07:38 -0500

    Modified the tree view to handel three view - All view, Recursive and local view. Implemented hash map to keep track. Removed the showAllButton cause toggle button replaces the functionality of it. And in tree view now only there are only folders which have images and only show images.

--- phase1/src/viewer/OpenFile.java
+++ phase1/src/viewer/OpenFile.java
@@ -2,9 +2,7 @@ package viewer;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
@@ -31,6 +29,7 @@ import Controller.Main;
 import model.ImageFile;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import model.ImageFileManager;
 
 
 public final class OpenFile{
@@ -40,42 +39,76 @@ public final class OpenFile{
 
 
     public void openFile(File file,Stage stage) {
+        final int[] toggleViewer = {-1};
+
         window = stage;
         window.setTitle("Lets do Tagging");
 
         GridPane newGrid = new GridPane();
-        newGrid.setAlignment(Pos.CENTER_LEFT);
+        newGrid.setAlignment(Pos.CENTER);
         newGrid.setPadding(new Insets(10, 10, 10, 10));
         newGrid.setVgap(10);
         newGrid.setHgap(10);
 
+        ImageFileManager imageFileManager = new ImageFileManager(file);
+        ImageFile[] imageList = imageFileManager.getAllImageFiles();
+        Map<String, ImageFile> fileMap = new HashMap<>();
         TreeView<String> a = new TreeView<>();
+        for(ImageFile imageFile: imageList) {
+            fileMap.put(imageFile.toString(), imageFile);
+        }
         GridPane.setConstraints(a,0,0);
-        a.setRoot(getNodesForDirectory(file));
+        TreeItem<String> tree = getNodesForDirectory(file, Arrays.toString(imageList));
+        TreeItem<String> holderTree = new TreeItem<>(file.getName() + " (Tree View)");
+        holderTree.getChildren().addAll(tree.getChildren());
+        tree = holderTree;
+
+        a.setRoot(tree);
         a.getSelectionModel().selectedItemProperty()
                 .addListener((v, oldValue, newValue) -> {
-                    if (newValue != null)
-                        System.out.println(newValue.getValue());
+                    if (newValue != null) {
+//                        System.out.println(newValue.getValue());
+                        System.out.println(fileMap.get(newValue.getValue()));
+                    }
                 });
 
         // button that shows all the image under the the selected directory
 
-        Button toggle = new Button("Change Toggle");
+        Button toggle = new Button("Toggle View");
+        GridPane.setConstraints(toggle,2,5);
+        toggle.setOnAction(e -> {
+            if(toggleViewer[0] == -1) {
+                toggleViewer[0] += 1;
+                TreeItem<String> newTree = new TreeItem<>(file.getName() +" (Recursive View)");
 
-//        newGrid.setAlignment(Pos.BASELINE_RIGHT);
-        Button showAll = new Button("Show all Images");
-        GridPane.setConstraints(showAll,5,5);
+//                TreeItem<String> tempTree = new TreeItem<>(file.getName() + " (All View)");
+//                tempTree.getChildren().addAll(newTree.getChildren());
+//                newTree = tempTree;
+                for(ImageFile imageFile: imageFileManager.getAllImageFiles()) {
+                    newTree.getChildren().add(new TreeItem<>(imageFile.getName()));
+                }
+                a.setRoot(newTree);
+            } else if(toggleViewer[0] == 0) {
+                toggleViewer[0] += 1;
+                TreeItem<String> newTree = new TreeItem<>(file.getName() +" (Local View)");
 
-//        TableView<ImageFile> table = new TableView<>();
-        System.out.println(file.getPath());
+                for(ImageFile imageFile: imageFileManager.getLocalImageFiles()) {
+                    newTree.getChildren().add(new TreeItem<>(imageFile.getName()));
+                }
+                a.setRoot(newTree);
+            } else if (toggleViewer[0] == 1) {
+                toggleViewer[0] = -1;
+                TreeItem<String> newTree = getNodesForDirectory(file, Arrays.toString(imageList));
+                TreeItem<String> tempTree = new TreeItem<>(file.getName() + " (Tree View)");
+                tempTree.getChildren().addAll(newTree.getChildren());
+                newTree = tempTree;
+                a.setRoot(newTree);
 
-        Main controller = new Main();
-        ImageFile[] imageList = controller.getImageFiles(controller.startProgram(file.getPath()));
-        showAll.setOnAction( e -> {
-            showImage(imageList, file.getPath());
+            }
         });
 
-        newGrid.getChildren().addAll(showAll);
+
+        newGrid.getChildren().addAll(a, toggle);
         Scene scene = new Scene(newGrid, 1020, 720);
         window.setScene(scene);
         window.show();
@@ -83,13 +116,20 @@ public final class OpenFile{
 
 
     // for making the
-    private TreeItem<String> getNodesForDirectory(File directory) {
+    private TreeItem<String> getNodesForDirectory(File directory, String StringThatContains) {
         TreeItem<String> root = new TreeItem<>(directory.getName());
         for(File f : directory.listFiles()) {
             if(f.isDirectory()) {
-                root.getChildren().add(getNodesForDirectory(f));
-            } else {
-                root.getChildren().add(new TreeItem<>(f.getName()));
+                TreeItem<String> children = getNodesForDirectory(f, StringThatContains);
+//                System.out.println(children.getChildren().size());
+                if(children.getChildren().size() != 0) {
+                    root.getChildren().add(getNodesForDirectory(f, StringThatContains));
+                }
+            }
+            else {
+                if(StringThatContains.contains(f.getName())) {
+                    root.getChildren().add(new TreeItem<>(f.getName()));
+                }
             }
         }
         return root;
