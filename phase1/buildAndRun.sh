84cf1dd630ef2afaa24eac1edac45a7f94464b10
Elliot Ford <elliot.ford@mail.utoronto.ca>
2017-11-19 03:07:01 -0500

    Created a bash script that builds and runs the project.

---
+++ phase1/buildAndRun.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Check if the out/production/phase1 folder exists and create it if it doesn't
+if [ ! -d ./out/production/phase1 ]; then
+    mkdir ./out/production/phase1/
+fi
+
+# build the entire project and put it in the out/production/phase1 folder
+echo "building project"
+javac -d ./out/production/phase1 ./src/*/*.java
+
+# run the project
+echo "starting project"
+java -cp ./out/production/phase1 viewer.Console
