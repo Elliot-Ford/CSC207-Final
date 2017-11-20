de5ae54d38b657a6593c4243f790ed2c7e951e96
Elliot Ford <elliot.ford@mail.utoronto.ca>
2017-11-19 20:21:52 -0500

    formatted everything according to design.

--- phase1/src/viewer/OpenFile.java
+++ phase1/src/viewer/OpenFile.java
@@ -16,7 +16,9 @@ import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
@@ -36,26 +38,31 @@ public final class OpenFile{
 
 
     public void openFile(File file,Stage stage) throws Exception {
-        final int[] toggleViewer = {-1};
-        final Console console = new Console();
-        final Map<String, ImageFile>[] fileMap = new Map[]{new HashMap<>()};
+        int[] toggleViewer = {-1};
+        Console console = new Console();
+        Map<String, ImageFile> fileMap = new HashMap<>();
 
         window = stage;
         window.setTitle("Lets do Tagging");
 
         GridPane newGrid = new GridPane();
-        newGrid.setAlignment(Pos.CENTER);
+        newGrid.setAlignment(Pos.TOP_LEFT);
         newGrid.setPadding(new Insets(10, 10, 10, 10));
         newGrid.setVgap(10);
         newGrid.setHgap(10);
 
+        VBox vBox = new VBox();
+        HBox buttonsBox = new HBox();
+        buttonsBox.setSpacing(10);
+
         ImageFileManager imageFileManager = new ImageFileManager(file);
-        ImageFile[] imageList = imageFileManager.getAllImageFiles();
         TreeView<String> a = new TreeView<>();
         for(ImageFile imageFile: imageFileManager.getAllImageFiles()) {
-            fileMap[0].put(imageFile.toString(), imageFile);
+            fileMap.put(imageFile.toString(), imageFile);
         }
-        GridPane.setConstraints(a,0,0);
+        vBox.setAlignment(Pos.TOP_LEFT);
+        vBox.getChildren().add(a);
+        //GridPane.setConstraints(a,0,0);
         final TreeItem<String>[] tree = new TreeItem[]{getNodesForDirectory(file, Arrays.toString(imageFileManager.getAllImageFiles()))};
         final TreeItem<String>[] holderTree = new TreeItem[]{new TreeItem<>(file.getName() + " (Tree View)")};
         holderTree[0].getChildren().addAll(tree[0].getChildren());
@@ -82,12 +89,12 @@ public final class OpenFile{
                 console.start(stage);
         });
 
-        GridPane.setConstraints(browseFiles,1,5);
+        //GridPane.setConstraints(browseFiles,1,5);
 
         // button that shows all the image under the the selected directory
 
         Button toggle = new Button("Toggle View");
-        GridPane.setConstraints(toggle,2,5);
+        //GridPane.setConstraints(toggle,2,5);
         toggle.setOnAction(e -> {
             if(toggleViewer[0] == -1) {
                 toggleViewer[0] += 1;
@@ -110,7 +117,7 @@ public final class OpenFile{
                 a.setRoot(newTree);
             } else if (toggleViewer[0] == 1) {
                 toggleViewer[0] = -1;
-                TreeItem<String> newTree = getNodesForDirectory(file, Arrays.toString(imageList));
+                TreeItem<String> newTree = getNodesForDirectory(file, Arrays.toString(imageFileManager.getAllImageFiles()));
                 TreeItem<String> tempTree = new TreeItem<>(file.getName() + " (Tree View)");
                 tempTree.getChildren().addAll(newTree.getChildren());
                 newTree = tempTree;
@@ -125,7 +132,9 @@ public final class OpenFile{
             // TODO: 19-11-2017 create a logger
         });
 
-        newGrid.getChildren().addAll(a, toggle, browseFiles);
+        buttonsBox.getChildren().addAll(toggle,browseFiles);
+        newGrid.getChildren().addAll(vBox);
+        vBox.getChildren().add(buttonsBox);
         Scene scene = new Scene(newGrid, 1020, 720);
         window.setScene(scene);
         window.show();
