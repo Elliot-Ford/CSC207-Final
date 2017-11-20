62e7e45d5948b057cb705db4c56b8728e996ee15
shlok <shlok.somani@mail.utoronto.ca>
2017-11-19 23:46:19 -0500

    Merge branch 'shlok' of C:\Users\shreeji\Desktop\Project\group_0508 with conflicts.

--- phase1/src/viewer/OpenFile.java
+++ phase1/src/viewer/OpenFile.java
@@ -1,34 +1,29 @@
 package viewer;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.stream.Collectors;
 
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.geometry.VPos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
 import javafx.scene.control.Button;
-import javafx.scene.control.TableView;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-import javafx.scene.control.TreeItem;
-import javafx.scene.control.TreeView;
 
-import Controller.Main;
 import model.ImageFile;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import model.ImageFileManager;
 
 
@@ -36,54 +31,70 @@ public final class OpenFile{
 
     Stage window;
 
+    private static void open(Stage stage) {
+        Application.launch(Console.class);
+    }
 
 
-    public void openFile(File file,Stage stage) {
-        final int[] toggleViewer = {-1};
+    public void openFile(File file,Stage stage) throws Exception {
+        int[] toggleViewer = {-1};
+        Console console = new Console();
+        Map<String, ImageFile> fileMap = new HashMap<>();
 
         window = stage;
         window.setTitle("Lets do Tagging");
 
         GridPane newGrid = new GridPane();
-        newGrid.setAlignment(Pos.CENTER);
+        newGrid.setAlignment(Pos.TOP_LEFT);
         newGrid.setPadding(new Insets(10, 10, 10, 10));
         newGrid.setVgap(10);
         newGrid.setHgap(10);
 
+        VBox vBox = new VBox();
+        HBox buttonsBox = new HBox();
+        buttonsBox.setSpacing(10);
+        buttonsBox.setPadding(new Insets(10, 10, 10, 10));
+
         ImageFileManager imageFileManager = new ImageFileManager(file);
-        ImageFile[] imageList = imageFileManager.getAllImageFiles();
-        Map<String, ImageFile> fileMap = new HashMap<>();
-        TreeView<String> a = new TreeView<>();
-        for(ImageFile imageFile: imageList) {
+
+        TreeView<String> fileViewer = new TreeView<>();
+        for(ImageFile imageFile: imageFileManager.getAllImageFiles()) {
             fileMap.put(imageFile.toString(), imageFile);
         }
-        GridPane.setConstraints(a,0,0);
-        TreeItem<String> tree = getNodesForDirectory(file, Arrays.toString(imageList));
-        TreeItem<String> holderTree = new TreeItem<>(file.getName() + " (Tree View)");
-        holderTree.getChildren().addAll(tree.getChildren());
-        tree = holderTree;
+        vBox.setAlignment(Pos.TOP_LEFT);
+        vBox.getChildren().add(fileViewer);
+        //GridPane.setConstraints(a,0,0);
+        final TreeItem<String>[] tree = new TreeItem[]{getNodesForDirectory(file, Arrays.toString(imageFileManager.getAllImageFiles()))};
+        final TreeItem<String>[] holderTree = new TreeItem[]{new TreeItem<>(file.getName() + " (Tree View)")};
+        holderTree[0].getChildren().addAll(tree[0].getChildren());
+        tree[0] = holderTree[0];
 
-        a.setRoot(tree);
-        a.getSelectionModel().selectedItemProperty()
+
+
+        fileViewer.setRoot(tree[0]);
+        fileViewer.getSelectionModel().selectedItemProperty()
                 .addListener((v, oldValue, newValue) -> {
                     if (newValue != null) {
-//                        System.out.println(newValue.getValue());
-                        System.out.println(fileMap.get(newValue.getValue()));
+                        if (newValue.getValue() != null){
+                            if (fileMap.containsKey(newValue.getValue())){
+                                 ImageFile imageFile = fileMap.get(newValue.getValue());
+                            }
+                    }
                     }
                 });
         // button for browsing between directories
+
         Button browseFiles = new Button("Change Directory");
-        browseFiles.setOnAction( e -> {
-            window.close();
-            //link it to the console
+        browseFiles.setOnAction((final ActionEvent e) -> {
+                console.start(stage);
         });
 
-        GridPane.setConstraints(browseFiles,1,5);
+        //GridPane.setConstraints(browseFiles,1,5);
 
         // button that shows all the image under the the selected directory
 
         Button toggle = new Button("Toggle View");
-        GridPane.setConstraints(toggle,2,5);
+        //GridPane.setConstraints(toggle,2,5);
         toggle.setOnAction(e -> {
             if(toggleViewer[0] == -1) {
                 toggleViewer[0] += 1;
@@ -95,7 +106,7 @@ public final class OpenFile{
                 for(ImageFile imageFile: imageFileManager.getAllImageFiles()) {
                     newTree.getChildren().add(new TreeItem<>(imageFile.getName()));
                 }
-                a.setRoot(newTree);
+                fileViewer.setRoot(newTree);
             } else if(toggleViewer[0] == 0) {
                 toggleViewer[0] += 1;
                 TreeItem<String> newTree = new TreeItem<>(file.getName() +" (Local View)");
@@ -103,78 +114,53 @@ public final class OpenFile{
                 for(ImageFile imageFile: imageFileManager.getLocalImageFiles()) {
                     newTree.getChildren().add(new TreeItem<>(imageFile.getName()));
                 }
-                a.setRoot(newTree);
+                fileViewer.setRoot(newTree);
             } else if (toggleViewer[0] == 1) {
                 toggleViewer[0] = -1;
-                TreeItem<String> newTree = getNodesForDirectory(file, Arrays.toString(imageList));
+                TreeItem<String> newTree = getNodesForDirectory(file, Arrays.toString(imageFileManager.getAllImageFiles()));
                 TreeItem<String> tempTree = new TreeItem<>(file.getName() + " (Tree View)");
                 tempTree.getChildren().addAll(newTree.getChildren());
                 newTree = tempTree;
-                a.setRoot(newTree);
+                fileViewer.setRoot(newTree);
 
             }
         });
 
+//        //logger to display all the changes ever done to any image
+//        Button log = new Button("Log");
+//        log.setOnAction(e -> {
+//            // TODO: 19-11-2017 create a logger
+//        });
 
-        newGrid.getChildren().addAll(a, toggle, browseFiles);
+        buttonsBox.getChildren().addAll(toggle,browseFiles);
+        newGrid.getChildren().addAll(vBox);
+        vBox.getChildren().add(buttonsBox);
         Scene scene = new Scene(newGrid, 1020, 720);
         window.setScene(scene);
+        window.sizeToScene();
         window.show();
     }
 
 
-    // for making the
+    // for making the tree
     private TreeItem<String> getNodesForDirectory(File directory, String StringThatContains) {
         TreeItem<String> root = new TreeItem<>(directory.getName());
-        for(File f : directory.listFiles()) {
-            if(f.isDirectory()) {
-                TreeItem<String> children = getNodesForDirectory(f, StringThatContains);
+        if(directory.listFiles() != null) {
+            for (File f : directory.listFiles()) {
+                if (f.isDirectory()) {
+                    TreeItem<String> children = getNodesForDirectory(f, StringThatContains);
 //                System.out.println(children.getChildren().size());
-                if(children.getChildren().size() != 0) {
-                    root.getChildren().add(getNodesForDirectory(f, StringThatContains));
-                }
-            }
-            else {
-                if(StringThatContains.contains(f.getName())) {
-                    root.getChildren().add(new TreeItem<>(f.getName()));
+                    if (children.getChildren().size() != 0) {
+                        root.getChildren().add(getNodesForDirectory(f, StringThatContains));
+                    }
+                } else {
+                    if (StringThatContains.contains(f.getName())) {
+                        root.getChildren().add(new TreeItem<>(f.getName()));
+                    }
                 }
             }
         }
         return root;
     }
 
-    private void showImage(ImageFile[] list, String path){
-        /**
-        TreeItem<String> nodeItem = new TreeItem<>();
-        for (ImageFile image : list){
-            nodeItem.getChildren().addAll((Collection<? extends TreeItem<String>>) image);
-         }
-         */
-        TreeItem<String> rootItem = new TreeItem<>("Root Folder");
-        rootItem.setExpanded(true);
-
-
-        window = new Stage();
-        window.setTitle("All Image");
-        GridPane newGrid = new GridPane();
-        newGrid.setPadding(new Insets(10, 10, 10, 10));
-        newGrid.setVgap(10);
-        newGrid.setHgap(10);
-
-        for (ImageFile IFile : list) {
-            //Node nodeIcon = new ImageView(new Image(getClass().getResourceAsStream(IFile.getName())));
-            TreeItem<String> nodeItem = new TreeItem<>(IFile.getName());
-            rootItem.getChildren().add(nodeItem);
-        }
-
-        TreeView<String> tree = new TreeView<>(rootItem);
-        StackPane root = new StackPane();
-        root.getChildren().add(tree);
-
-        newGrid.getChildren().addAll(root.getChildren());
-
-        Scene scene = new Scene(newGrid, 1020, 720);
-        window.setScene(scene);
-        window.showAndWait();
-    }
 }
