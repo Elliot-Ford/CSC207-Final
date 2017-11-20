a0d456b96dc07b2e7434cbc174688e638a7e2608
shlok <shlok.somani@mail.utoronto.ca>
2017-11-19 19:03:48 -0500

    Added Buttons to editImages class and modified openFile. openFile still need to be reconstructed to implement editImage and display everything in the same stage

--- phase1/src/viewer/OpenFile.java
+++ phase1/src/viewer/OpenFile.java
@@ -12,19 +12,18 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.geometry.VPos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
-import javafx.scene.control.TreeItem;
-import javafx.scene.control.TreeView;
 
 import Controller.Main;
 import model.ImageFile;
@@ -50,7 +49,7 @@ public final class OpenFile{
         window.setTitle("Lets do Tagging");
 
         GridPane newGrid = new GridPane();
-        newGrid.setAlignment(Pos.CENTER);
+//        newGrid.setAlignment(Pos.TOP_LEFT);
         newGrid.setPadding(new Insets(10, 10, 10, 10));
         newGrid.setVgap(10);
         newGrid.setHgap(10);
@@ -62,22 +61,29 @@ public final class OpenFile{
         for(ImageFile imageFile: imageList) {
             fileMap.put(imageFile.toString(), imageFile);
         }
+//        newGrid.setAlignment(Pos.CENTER_LEFT);
         GridPane.setConstraints(a,0,0);
         TreeItem<String> tree = getNodesForDirectory(file, Arrays.toString(imageList));
         TreeItem<String> holderTree = new TreeItem<>(file.getName() + " (Tree View)");
         holderTree.getChildren().addAll(tree.getChildren());
         tree = holderTree;
 
-        editImage ei = new editImage();
+
 
         a.setRoot(tree);
         a.getSelectionModel().selectedItemProperty()
                 .addListener((v, oldValue, newValue) -> {
                     if (newValue != null) {
-                        ei.editPic(newValue);
+                        if (newValue.getValue() != null){
+                            if (fileMap.containsKey(newValue.getValue())){
+                                 ImageFile imageFile = fileMap.get(newValue.getValue());
+
+                            }
+                        }
                     }
                 });
         // button for browsing between directories
+
         Button browseFiles = new Button("Change Directory");
         browseFiles.setOnAction( e -> {
             OpenFile.open(window);
@@ -89,6 +95,7 @@ public final class OpenFile{
         // button that shows all the image under the the selected directory
 
         Button toggle = new Button("Toggle View");
+//        newGrid.setAlignment(Pos.TOP_LEFT);
         GridPane.setConstraints(toggle,2,5);
         toggle.setOnAction(e -> {
             if(toggleViewer[0] == -1) {
@@ -121,7 +128,13 @@ public final class OpenFile{
             }
         });
 
+        //logger to display all the changes ever done to any image
+        Button log = new Button("Log");
+        log.setOnAction(e -> {
+            // TODO: 19-11-2017 create a logger
+        });
 
+//        newGrid.setAlignment(Pos.TOP_LEFT);
         newGrid.getChildren().addAll(a, toggle, browseFiles);
         Scene scene = new Scene(newGrid, 1020, 720);
         window.setScene(scene);
@@ -129,7 +142,7 @@ public final class OpenFile{
     }
 
 
-    // for making the
+    // for making the tree
     private TreeItem<String> getNodesForDirectory(File directory, String StringThatContains) {
         TreeItem<String> root = new TreeItem<>(directory.getName());
         if(directory.listFiles() != null) {
@@ -149,41 +162,5 @@ public final class OpenFile{
         }
         return root;
     }
-
-    private void showImage(ImageFile[] list, String path){
-        /**
-        TreeItem<String> nodeItem = new TreeItem<>();
-        for (ImageFile image : list){
-            nodeItem.getChildren().addAll((Collection<? extends TreeItem<String>>) image);
-         }
-         */
-        TreeItem<String> rootItem = new TreeItem<>("Root Folder");
-        rootItem.setExpanded(true);
-
-
-        window = new Stage();
-        window.setTitle("All Image");
-        GridPane newGrid = new GridPane();
-        newGrid.setPadding(new Insets(10, 10, 10, 10));
-        newGrid.setVgap(10);
-        newGrid.setHgap(10);
-
-        for (ImageFile IFile : list) {
-            //Node nodeIcon = new ImageView(new Image(getClass().getResourceAsStream(IFile.getName())));
-            TreeItem<String> nodeItem = new TreeItem<>(IFile.getName());
-            rootItem.getChildren().add(nodeItem);
-        }
-
-        TreeView<String> tree = new TreeView<>(rootItem);
-        StackPane root = new StackPane();
-        root.getChildren().add(tree);
-
-        newGrid.getChildren().addAll(root.getChildren());
-
-        Scene scene = new Scene(newGrid, 1020, 720);
-        window.setScene(scene);
-        window.showAndWait();
-    }
-
 
 }
