88c175be978d97c818d005b015a64808c2bc7952
Elliot Ford <elliot.ford@mail.utoronto.ca>
2017-11-20 01:10:13 -0500

    got basics of viewer to work.

--- phase1/src/viewer/Viewer.java
+++ phase1/src/viewer/Viewer.java
@@ -1,4 +1,25 @@
 package viewer;
 
-public class Viewer {
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Viewer extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("viewer.fxml"));
+
+        Scene scene = new Scene(root, 900, 600);
+
+        stage.setTitle("Tagger");
+        stage.setScene(scene);
+        stage.show();
+    }
 }
--- phase1/src/viewer/ViewerController.java
+++ phase1/src/viewer/ViewerController.java
@@ -1,4 +1,34 @@
 package viewer;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+import java.awt.event.ActionEvent;
+
 public class ViewerController {
+
+    @FXML
+    public ListView avaliableTags;
+    public Button createTag;
+    public ImageView Image;
+    public ListView previousTags;
+    public ListView currentTags;
+    public TextField tagToAdd;
+    public ListView log;
+    //public Button toggleViewer;
+    public Button changeDir;
+    public Button move;
+    public Button removeTag;
+    public Button addTag;
+    public Button deleteTag;
+    public ImageView image;
+
+
+    @FXML public void handleToggleViewerAction(ActionEvent event) {
+    System.out.println("dog" );
+    }
+
 }
