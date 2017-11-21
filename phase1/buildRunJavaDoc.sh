ea8cb0a4a1c2e5b176f1113105bc65337f3544cf
Elliot Ford <elliot.ford@mail.utoronto.ca>
2017-11-21 06:15:41 -0500

    Added design.pdf Added help.txt renamed buildAndRun.sh to buildRunJavaDoc.sh

--- phase1/buildRunJavaDoc.sh
+++ phase1/buildRunJavaDoc.sh
@@ -5,6 +5,13 @@ if [ ! -d ./out/production/phase1 ]; then
     mkdir ./out/production/phase1/
 fi
 
+#Generate the javaDocs for the project
+echo "generating javaDoc"
+if [ ! -d ./javaDoc ]; then
+	mkdir ./javaDoc
+fi
+javadoc -d ./javaDoc -cp src/ model viewer
+
 # build the entire project and put it in the out/production/phase1 folder
 echo "building project"
 javac -d ./out/production/phase1 ./src/*/*.java
