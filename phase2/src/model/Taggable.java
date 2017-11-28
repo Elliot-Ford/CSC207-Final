972382a10480591e414a199d0d39a183e4eae5a2
Elliot Ford <elliot.ford@mail.utoronto.ca>
2017-11-28 03:41:44 -0500

    added more methods to Taggable interface and reformatted it.

--- phase2/src/model/Taggable.java
+++ phase2/src/model/Taggable.java
@@ -2,7 +2,9 @@ package model;
 
 /** Represents all classes that can have tags added and removed from it */
 public interface Taggable {
-    boolean addTag(String tag);
-    boolean removeTag(String tag);
+  boolean addTag(String tag);
 
+  boolean removeTag(String tag);
+
+  String[] getTags();
 }
