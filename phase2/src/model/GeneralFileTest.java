5e736afbb1ae32e8bd41d8452a3c0894e5165c29
Elliot Ford <elliot.ford@mail.utoronto.ca>
2017-11-30 18:35:37 -0500

    Created GeneralFileTest to test general file.

---
+++ phase2/src/model/GeneralFileTest.java
@@ -0,0 +1,93 @@
+package model;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GeneralFileTest {
+  @Before
+  public void setUp() throws Exception {}
+
+  @After
+  public void tearDown() throws Exception {}
+
+  @Test
+  public void getImage() throws Exception {}
+
+  @Test
+  public void moveFile() throws Exception {}
+
+  @Test
+  public void getTags() throws Exception {}
+
+  @Test
+  public void getPreviousTags() throws Exception {}
+
+  @Test
+  public void addTag() throws Exception {}
+
+  @Test
+  public void removeTag() throws Exception {}
+
+  @Test
+  public void rename() throws Exception {}
+
+  @Test
+  public void getName() throws Exception {}
+
+  @Test
+  public void testToString() throws Exception {}
+
+  @Test
+  public void getImage1() throws Exception {}
+
+  @Test
+  public void getActualName() throws Exception {}
+
+  @Test
+  public void getFile() throws Exception {}
+
+  @Test
+  public void equals() throws Exception {}
+
+  @Test
+  public void update() throws Exception {}
+
+  @Test
+  public void getLog() throws Exception {}
+
+  @Test
+  public void getPreviousStates() throws Exception {}
+
+  @Test
+  public void testHashCode() throws Exception {}
+
+  @Test
+  public void addObserver() throws Exception {}
+
+  @Test
+  public void deleteObserver() throws Exception {}
+
+  @Test
+  public void notifyObservers() throws Exception {}
+
+  @Test
+  public void notifyObservers1() throws Exception {}
+
+  @Test
+  public void deleteObservers() throws Exception {}
+
+  @Test
+  public void setChanged() throws Exception {}
+
+  @Test
+  public void clearChanged() throws Exception {}
+
+  @Test
+  public void hasChanged() throws Exception {}
+
+  @Test
+  public void countObservers() throws Exception {}
+}
