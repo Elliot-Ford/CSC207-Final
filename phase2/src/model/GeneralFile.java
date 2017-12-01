6e8c769b250360f3ee2257f5b790147818a32969
shlok <shlok.somani@mail.utoronto.ca>
2017-11-30 21:45:06 -0500

    Completed the java docs

--- phase2/src/model/GeneralFile.java
+++ phase2/src/model/GeneralFile.java
@@ -6,14 +6,26 @@ import java.io.File;
 
 public class GeneralFile extends AbsTaggableFile {
 
+  /**
+   * Constructs a new GeneralFile object from a string
+   * @param path
+   */
   public GeneralFile(String path) {
     this(new File(path));
   }
 
+  /**
+   * Constructs a new GeneralFile object from the given file
+   * @param file
+   */
   public GeneralFile(File file) {
     super(file);
   }
 
+  /**
+   * returns the associated image.
+   * @return Image
+   */
   @Override
   public Image getImage() {
     return null;
--- phase2/src/model/Taggable.java
+++ phase2/src/model/Taggable.java
@@ -2,8 +2,21 @@ package model;
 
 /** Represents all classes that can have tags added and removed from it */
 public interface Taggable {
+
+  /**
+   * Add tags to the file
+   * @param tags
+   * @return boolean
+   * @throws Exception
+   */
   boolean addTag(String[] tags) throws Exception;
 
+  /**
+   * removes tag from a given file
+   * @param tags
+   * @return
+   * @throws Exception
+   */
   boolean removeTag(String[] tags) throws Exception;
 
   String[] getTags();
